87c6e2d86ac91d5f960fa0f13960e0721ad3b7f2
s770am <s770am@gmail.com>
2021-09-30 22:32:51 -0500

    seesions up just not path or the require

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def current_team_member
+        @current_team_member = Team_member.find(session[:user_id]) if session[:user_id]
+    end
 end
