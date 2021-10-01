97b372765f9cc7fdfb5f248ea631c9e06770079a
AaronPacht <aharonpacht@gmail.com>
2021-10-01 03:00:01 -0400

    contact show, links from show page, destroy contact,note-index,create,update,destroy

---
+++ test/controllers/notes_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class NotesControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get notes_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get notes_destroy_url
+    assert_response :success
+  end
+end
