a43727e14912aecda203f9686a38446f96f080d5
AaronPacht <aharonpacht@gmail.com>
2021-10-03 00:02:07 -0400

    validations for new team member and job input

--- app/javascript/packs/team_member_index.js
+++ app/javascript/packs/team_member_index.js
@@ -1,56 +1,56 @@
 document.addEventListener('DOMContentLoaded',(e)=>{
-    let current=-1
-    let currentMail=-1
-    let showlinks=document.querySelectorAll('.team-member-list')
-    hidden=document.querySelectorAll('.hidden')
+    let current=-1;
+    let currentMail=-1;
+    let showlinks=document.querySelectorAll('.team-member-list');
+    hidden=document.querySelectorAll('.hidden');
     for (let index = 0; index < showlinks.length; index++) {
         let member=document.querySelectorAll('.team-member-list')[index];
         member.addEventListener('click',(e)=>{
             // close open 'show'
             if(current>=0){
-                hidden[current].style.display="none"
-            }
+                hidden[current].style.display="none";
+            };
             // close open 'new message'
             if(currentMail>=0){
-                hiddenMail[currentMail].style.display="none"
-            }
+                hiddenMail[currentMail].style.display="none";
+            };
             // unhide 'show' for that team member
-            hidden[index].style.display='unset'
+            hidden[index].style.display='unset';
             // if that 'show' is open
             if(current===index){
                 // close 'show'
-                hidden[index].style.display='none'
-                current=-1
+                hidden[index].style.display='none';
+                current=-1;
             }else{
                 // keep track of open 'show'
-                current=index
-            }
-        })
-    }
-    let messageBtn=document.querySelectorAll('#mail')
-    hiddenMail=document.querySelectorAll('.hidden-add')
+                current=index;
+            };
+        });
+    };
+    let messageBtn=document.querySelectorAll('#mail');
+    hiddenMail=document.querySelectorAll('.hidden-add');
     for (let index = 0; index < messageBtn.length; index++) {
         let message=document.querySelectorAll('#mail')[index];
         message.addEventListener('click',(e)=>{
             // close open 'new message'
             if(currentMail>=0){
-                hiddenMail[currentMail].style.display="none"
-            }
+                hiddenMail[currentMail].style.display="none";
+            };
             // close open 'show'
             if(current>=0){
-                hidden[current].style.display="none"
-            }
+                hidden[current].style.display="none";
+            };
             // unhide 'new message' for that team member
-            hiddenMail[index].style.display='unset'
+            hiddenMail[index].style.display='unset';
             // if that 'new message' is open
             if(currentMail===index){
                 // close 'new message'
-                hiddenMail[index].style.display='none'
-                currentMail=-1
+                hiddenMail[index].style.display='none';
+                currentMail=-1;
             }else{
                 // keep track of open 'new message'
-                currentMail=index
-            }
-        })
-    }
-})
+                currentMail=index;
+            };
+        });
+    };
+});
--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -10,6 +10,12 @@ class TeamMember < ApplicationRecord
 
     validates :password, length: { minimum: 5 }
     validates :email, uniqueness: true
+    validates :email, presence: true
+    validates :name, presence: true
+    validates :job, presence: true
+    validates :password, presence: true
+    validates :password_confirmation, presence: true
+    validates :team_id, presence: true
     
 
 end
